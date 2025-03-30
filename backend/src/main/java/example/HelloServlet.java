8b2a1864accc45d310dc25c31802dd46e4d0f42f
yuta-komura <esfgshdfsd@gmail.com>
2025-03-30 23:01:06 +0900

    commit

--- backend/src/main/java/example/HelloServlet.java
+++ backend/src/main/java/example/HelloServlet.java
@@ -10,7 +10,6 @@ public class HelloServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    req.setAttribute("message", "Hello from Servlet");
     req.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(req, resp);
   }
 }
